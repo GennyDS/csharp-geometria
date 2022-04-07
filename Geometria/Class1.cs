d4ae168235e3b19f7f3575727775902c133eda02
Gennaro <gennarinodi02@gmail.com>
2022-04-07 15:35:49 +0200

    commit 1

---
+++ Geometria/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometria
+{
+    public class Rettangolo
+    {
+      private int Base;
+        private int altezza;
+        private  int Area;
+            private  int Perimetro;
+        public Rettangolo(int Base, int altezza) //costruttore
+        {
+            this.Base = Base;
+            this.altezza = altezza;
+        }
+
+       
+
+    public int CalcoloArea(int Base, int altezza)
+    {
+        int Area = Base * altezza;
+        return Area;
+    }
+    public int CalcoloPerimetro(int Base, int altezza)
+    {
+        int Perimetro = 2 * Base + 2 * altezza;
+        return Perimetro;
+    }
+
+    public void StampaRettangolo(  )
+    {
+         
+        Console.WriteLine("----- Rettangolo -------");
+        Console.WriteLine("AREA: " + Area);
+        Console.WriteLine("PERIMETRO: " + Perimetro);
+        Console.WriteLine("------------------");
+
+        }
+
+    }
+}
+
+
--- Geometria/Program.cs
+++ Geometria/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Geometria;
+
+Console.WriteLine("inserisci la base del rettangolo");
+int BaseRettangolo= int.Parse(Console.ReadLine());
+
+Console.WriteLine("inserisci l'altezza del rettangolo");
+int AltezzaRettangolo = int.Parse(Console.ReadLine());
+
+Rettangolo Rettangolo1 = new Rettangolo ( BaseRettangolo, AltezzaRettangolo);
+
+Rettangolo1.CalcoloArea(BaseRettangolo, AltezzaRettangolo);
+Rettangolo1.CalcoloPerimetro(BaseRettangolo, AltezzaRettangolo);
+Rettangolo1.StampaRettangolo();
